2169d9fb722ce6191de908557a77bcb8b5239230
andres-eric <andres-eric@users.noreply.github.com>
2026-08-10 16:53:27 -0500

    Update search result parsing in tool.py

--- Ollama Tools in LangChain/RE rank copy/tool.py
+++ Ollama Tools in LangChain/RE rank copy/tool.py
@@ -37,7 +37,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage
 from langchain_core.tools import tool
 from ddgs import DDGS
-from langchain.messages import HumanMessage, ToolMessage
+from langchain.messages import HumanMessage, ToolMessage, SystemMessage
 
 import warnings
 warnings.filterwarnings("ignore", category=ResourceWarning)
@@ -52,23 +52,33 @@ def google_search(query:str)->str:
     """Busca en internet usando DuckDuckGo Search y devuelve los resultados.
     Útil cuando necesitas buscar información actualizada, noticias o datos recientes."""
     try: 
-        resultados=DDGS().text(query, max_results=1)
-        texto_limpio="\n".join(f"URL: {res['href']}\nBody: {res['body']}"   for res in resultados )
-        print(texto_limpio)
+        resultados=DDGS().text(query, max_results=3)
+        texto_limpio="\n".join(f"Link de la noticia: {res['href']}\nNoticia: {res['body']}"   for res in resultados )
+        #print(texto_limpio)
         return texto_limpio
     except Exception as e:
         return f"Error al buscar en Google: {e}"
 
+@tool
+def caculadora_de_raices(numero:float)->float:
+    """Calcula la raíz cuadrada de un número."""
+
+    raiz=numero**0.5
+    if raiz>0:
+        return raiz
+    else:
+        raise ValueError("El número complejo no se puede calcular")
+    
 
 def tool_llm():
 
 
     try:
         llm_gemini_instance = ChatOllama(
-            model="qwen2.5",
+            model="llama3.1",
             base_url="http://localhost:11434",  # URL por defecto de Ollama
             temperature=0.0, 
-            repeat_penalty=1.15,
+            #repeat_penalty=1.15,
             #format="json"
             #num_gpu=1,
             #num_thread=12
@@ -97,30 +107,63 @@ def tool_llm():
     llm_with_tools = llm_gemini_instance.bind_tools([google_search])
 
 
-    result=llm_with_tools.invoke([HumanMessage(content="cuantos años tiene actualmente donald trump ")])
-    texto=[]
-    
-    if hasattr(result, "tool_calls"):
+    instrucciones_analista = SystemMessage(content=""""
+    Eres un analista Senior de Licitaciones B2B para una empresa de energía renovable.
+    Acabas de recibir los resultados de búsqueda en internet sobre licitaciones de paneles solares.
+
+    REGLAS DE ANÁLISIS:
+    1. Extrae únicamente las licitaciones que sean de Colombia.
+    2. Descarta cualquier noticia que sea solo informativa (buscamos contratos o compras reales).
+    3. Si el texto de internet viene basura o código, ignóralo y extrae solo lo legible.
+
+    FORMATO DE RESPUESTA ESTRICTO:
+    Para cada oportunidad válida, responde usando exactamente esta estructura:
+    🔸 ENTIDAD: [Nombre de la entidad, ej: Alcaldía de Bogotá]
+    🔸 DESCRIPCIÓN: [Resumen de 2 líneas de lo que se va a comprar]
+    🔸 EVALUACIÓN COMERCIAL: [Por qué deberíamos participar en esto]
+    🔸 LINK: https://www.youtube.com/watch?v=vw0m43ixSBs
+    """)
+
+
+
+
+    pregunta=HumanMessage(content="traeme licitaciones que tenga el estado colombiano o contrataciones acerca de paneles solares")
+
+    mensaje=[instrucciones_analista,pregunta]
+
+    result=llm_with_tools.invoke(mensaje)
+
+    mensaje.append(result)
+
+    if hasattr(result, "tool_calls") and len(result.tool_calls) > 0:
 
         for tool_call in result.tool_calls:
             print("\n--- INSTANCIA DE LLAMADA DETECTADA ---")
-            print(f"herramienta usa {tool_call['name']}")
-            print(f"los argumentos son {tool_call['args']}")
+            print("######################################")
+            #print(f"herramienta usa {tool_call['name']}")
+            #print(f"los argumentos son {tool_call['args']}")
 
-
+            # 1. Ejecutamos la herramienta para obtener la búsqueda
             busqueda=google_search.invoke({"query": tool_call['args']['query']})
 
-            print(f"\ Lo que encontró Python en internet:\n{busqueda}")
+            # 2. Iteramos cada noticia obtenida del resultado de la herramienta
+            noticias = busqueda.split("Link de la noticia: ")
+            conteo = 0
+            for noticia in noticias:
+                if noticia.strip():
+                    print(f"--- Noticia número {conteo} ---")
+                    print(f"Link de la noticia: {noticia.strip()}\n")
+                    conteo += 1
 
+            mensaje_herramienta = ToolMessage(content=busqueda, tool_call_id=tool_call["id"])
+            mensaje.append(mensaje_herramienta)
 
-            resultado_final = llm_with_tools.invoke([
-            HumanMessage(content=busqueda),
-            ToolMessage(content=busqueda, tool_call_id=tool_call["id"])
-            ])
+            print(f"\n---MODELO ANALIZANDO LOS DATOS ---")
 
-        
-    
-
+            resultado_final=llm_with_tools.invoke(mensaje)
+            
+            print("\n--- RESPUESTA FINAL DEL MODELO ---")
+            print(resultado_final.content)
 
     else:
         print("\n-NO SE LLAMÓ A LA HERRAMIENTA -")
@@ -132,10 +175,5 @@ def tool_llm():
 
 
 
-    
-    
-
-
 if __name__ == "__main__":
-    
    tool_llm()
